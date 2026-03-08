ffa82357a68053f5a4a148e798e460b579af1553
elias-afz <elias.afzalzada@ontariotechu.net>
2026-03-08 16:29:48 -0400

    Revert "Merge branch 'main' of https://github.com/akashsayanthan/ACHAE---Movie-Catalog"
    This reverts commit 6e5c140053539be0c4ca65cd68b89cb717b707ca, reversing
    changes made to a76cdb8a928fc7e8359baccb26ebc4f08a7bf1c9.

--- my-react-app/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import MovieCatalog from './MovieCatalog'
-// or if you put it in components/:
-// import MovieCatalog from './components/MovieCatalog'
-
-function App() {
-  return <MovieCatalog />
-}
-
-export default App
